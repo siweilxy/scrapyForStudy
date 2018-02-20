1cc5a476a78d6c2bef1842f99d168d83c159e955
siweilxy <siweilxy@163.com>
2018-02-20 12:15:15 +0800

    add filter
    Signed-off-by: siweilxy <siweilxy@163.com>

--- bugs/pipelines.py
+++ bugs/pipelines.py
@@ -20,18 +20,29 @@ class BugsPipeline(object):
         if 'title' in item and 'link' in item:
             title=item['title'][0]
             link=item['link'][0]
+            #logging.critical(title)
+            #logging.critical(link)
+            if re.match('http://my.csdn.net*',link) \
+                    or re.match('https://edu.csdn.net*',link) \
+                    or re.match('javascript*',link)\
+                    or re.match('http://ask.csdn.net*',link)\
+                    or re.match('http://www.csdn.net*',link):
+                #logging.critical("link is not needed")
+                return item
+            #if re.match('http://blog.csdn.net*',link):
+                #logging.critical("blog.csdn.net is needed")
             if title not in self.list:
                 self.list.add(title)
                 r = redis.Redis(host='192.168.1.16', port=6379, db=0)
                 logging.critical(item['title'][0])
                 r.set(item['title'][0], item['link'][0])  # 添加
+                return item
             #link = r.get(item['title'])
             #result = re.match("http", link)
             # if result==None:
             # logging.critical(link)  # 获取
             # logging.critical(item['title'])
             # logging.critical("+++++++++++++++++++++++++++++++++++++++ process end+++++++++++++++++++++++++++++++++++++++")
-                return item
         #elif 'title' in item:
             #logging.critical("+++++++++++++++++++++++++++++++++++++++1212")
             #logging.critical("测试")
