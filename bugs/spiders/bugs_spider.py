0115a49c76e0d7efedf68943e5dfbb4cd8f7a82e
siweilxy <siweilxy@163.com>
2018-02-20 11:08:07 +0800

    remove comment
    Signed-off-by: siweilxy <siweilxy@163.com>

--- bugs/spiders/bugs_spider.py
+++ bugs/spiders/bugs_spider.py
@@ -40,59 +40,6 @@ class DmozSpider(CrawlSpider):
         #logging.critical(l.load_item())
         #logging.critical("after parse")
         return l.load_item()
-"""""
-    def parse(self, response):
-        reload(sys)
-        sys.setdefaultencoding('utf-8')
-        #print("parse start:")
-        #filename = response.url.split("/")[-2]
-        items=[]
-        divs= response.xpath('//div')
-        for p in divs.xpath('.//a'):
-            url=p.xpath('@href').extract_first()
-            title=p.xpath('text()').extract_first()
-            if type(title) is types.NoneType:
-                continue
-            if type(url) is types.NoneType:
-                continue
-
-            title=title.strip()
-            url=url.strip()
-            if url==u'None':
-                #print('+++++++++++++++++++++++++1')
-                continue
-            if len(url)==0:
-                #print('++++++++++2')
-                continue
-            if title==u"":
-                continue
-            item=DmozItem()
-            item['title']=title
-            item['link']=url
-            items.append(item)
-            #res1=re.match("http",item['link'])
-            #res2=re.match("https",item['link'])
-            #if res1!=None or res2!=None:
-                #yield Request(urlparse.urljoin(response.url,url))
-                #yield self.parse_items(item)
-        #print 4
-            #else:
-                #print item['link']+'_______________________________________'
-        #print("parse end")
-        #yield set(items)
-        #return set(items)
-        for i in items:
-            res1=re.match("http",i['link'])
-            res2=re.match("https",i['link'])
-            if res1!=None or res2!=None:
-                print("add to url",i['link'],"text is ",i['title'])
-                yield Request(urlparse.urljoin(response.url,i['link']))
-        yield self.parse_items(items)
-
-    def parse_items(self,item):
-        print "return"
-        return set(item)
-        """
 
 
 
